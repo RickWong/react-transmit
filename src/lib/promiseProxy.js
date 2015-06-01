6c5048d98cdc823e12cb0d1e1d2e67f854daa336
Rick <wong@mollie.com>
2015-06-02 00:06:05 +0200

    Allow optional substitution of the Promise implementation. Defaults to `global.Promise`.

---
+++ src/lib/promiseProxy.js
@@ -0,0 +1,10 @@
+/**
+ * @copyright © 2015, Rick Wong. All rights reserved.
+ */
+"use strict";
+
+module.exports = {
+	Promise: global.Promise || function () {
+		throw new Error("Missing ES6 Promise implementation")
+	}
+};
