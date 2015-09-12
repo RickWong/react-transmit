5183c1e41621ee2e053b017c792be2464bd5f00a
Rick <wong@mollie.com>
2015-09-12 15:32:56 +0200

    Breaking upgrade to React 0.14-rc and ReactDOM 0.14-rc.

--- src/example.js
+++ src/example.js
@@ -3,9 +3,6 @@ import Transmit from "lib/react-transmit";
 import Main from "example/Main";
 
 /**
- * For this client-side only example just use React.render() since there is no pre-queried data.
  * For isomorphic rendering using Transmit.render() will automatically render with pre-queried data.
  */
-React.render(<Main />, document.getElementById("react-root"));
-
-// Transmit.render(Main, {}, document.getElementById("react-root"));
+Transmit.render(Main, {}, document.getElementById("react-root"));
